1fa27747eee983c8a61a429907b798b2b31c992d
1594627 <3371650332@qq.com>
2025-06-21 09:49:24 +0800

    产品管理模块和产品类型下拉框显示创建

---
+++ aspnet-core/src/CustomerRelationshipManagement.Domain/ProductManagement/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace CustomerRelationshipManagement.ProductManagement
+{
+    /// <summary>
+    /// 产品分类
+    /// </summary>
+    public class Category : FullAuditedAggregateRoot<Guid>
+    {
+        /// <summary>
+        /// 产品分类名称
+        /// </summary>
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
---
+++ aspnet-core/src/CustomerRelationshipManagement.Domain/ProductManagement/Product.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace CustomerRelationshipManagement.ProductManagement
+{
+    /// <summary>
+    /// 产品管理模块
+    /// </summary>
+    public class Product: FullAuditedAggregateRoot<Guid>
+    {
+        /// <summary>
+        /// 产品分类
+        /// </summary>
+        public int CategoryId { get; set; }
+        /// <summary>
+        /// 产品图片
+        /// </summary>
+        public string ProductImageUrl { get; set; }=string.Empty;
+        /// <summary>
+        /// 门幅
+        /// </summary>
+        public string ProductBrand { get; set; }= string.Empty;
+        /// <summary>
+        //// 供应商
+        /// </summary>
+        public string ProductSupplier { get; set; }= string.Empty;
+        /// <summary>
+        /// 产品编号
+        /// </summary>
+        public string ProductCode { get; set; } = string.Empty;
+ 
+        /// <summary>
+        /// 产品描述
+        /// </summary>
+        public string ProductDescription { get; set; } = string.Empty;
+        /// <summary>
+        /// 建议售价
+        /// </summary>
+        public decimal? SuggestedPrice { get; set; } = 0;
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string ProductRemark { get; set; } = string.Empty;
+        /// <summary>
+        /// 上架未上架
+        /// </summary>
+        public bool ProductStatus { get; set; }= false;
+        /// <summary>
+        /// 成交价
+        /// </summary>
+        public decimal? DealPrice { get; set; }=0;
+    }
+}
