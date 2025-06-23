5bcfd60311ef4129f10dae3a4b2beb511007f00c
118736635696 <3299178542@qq.com>
2025-06-23 15:40:03 +0800

    提交验证码

---
+++ aspnet-core/src/CustomerRelationshipManagement.Application.Contracts/Captchas/ICaptchaServer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerRelationshipManagement.Captchas
+{
+    public interface ICaptchaServer
+    {
+        IActionResult Captcha(string id);
+        /// <summary>
+        /// 多次校验（https://gitee.com/pojianbing/lazy-captcha/issues/I4XHGM）
+        /// 演示时使用HttpGet传参方便，这里仅做返回处理
+        /// </summary>
+        bool Validate2(string id, string code);
+    }
+}
