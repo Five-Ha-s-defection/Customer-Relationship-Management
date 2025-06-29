633e32c80bdf50d321607629617053f8210fdadc
118736635696 <3299178542@qq.com>
2025-06-29 20:48:38 +0800

    Revert "菜单提交"
    This reverts commit df6ce97ae4b8ccd9e132a64924dc21c08131ccd9.

--- aspnet-core/src/CustomerRelationshipManagement.Application/RBAC/Menus/MenuAppService.cs
+++ aspnet-core/src/CustomerRelationshipManagement.Application/RBAC/Menus/MenuAppService.cs
@@ -1,14 +1,15 @@
-using CustomerRelationshipManagement.ApiResults;
-using CustomerRelationshipManagement.Application.Contracts.RBAC.Menus;
-using CustomerRelationshipManagement.RBAC.Menus;
-using CustomerRelationshipManagement.RBACDtos.Menus;
-using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Volo.Abp.Application.Services;
+using CustomerRelationshipManagement.Application.Contracts.RBAC.Menus;
+using CustomerRelationshipManagement.RBACDtos.Menus;
+using CustomerRelationshipManagement.RBACDtos.Users;
+using CustomerRelationshipManagement.ApiResults;
+using CustomerRelationshipManagement.RBAC.Menus;
 using Volo.Abp.Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.Application.Services;
 
 namespace CustomerRelationshipManagement.Application.RBAC.Menus
 {
