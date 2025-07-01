4b04be3d6e51bbcc320053f0b05d737b965c52df
1594627 <3371650332@qq.com>
2025-07-01 16:26:58 +0800

    联系人所有操作

---
+++ aspnet-core/src/CustomerRelationshipManagement.Application.Contracts/DTOS/CustomerProcessDtos/CustomerContacts/CustomerContactDto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace CustomerRelationshipManagement.DTOS.CustomerProcessDtos.CustomerContacts
+{
+    public class CustomerContactDto : FullAuditedAggregateRoot<Guid>
+    {
+        /// <summary>
+        /// 所属客户ID（外键）
+        /// </summary>
+        public Guid CustomerId { get; set; }=Guid.Empty;
+
+        /// <summary>
+        /// 联系人姓名
+        /// </summary>
+        public string ContactName { get; set; }=string.Empty;
+
+        /// <summary>
+        /// 联系人关系（外键）
+        /// </summary>
+        public Guid ContactRelationId { get; set; } = Guid.Empty;
+
+        /// <summary>
+        /// 角色（外键）
+        /// </summary>
+        public Guid RoleId { get; set; } = Guid.Empty;
+
+        /// <summary>
+        /// 尊称（先生/女士）
+        /// </summary>
+        public bool Salutation { get; set; }= false;
+
+        /// <summary>
+        /// 职位
+        /// </summary>
+        public string Position { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 手机
+        /// </summary>
+        public string Mobile { get; set; } = string.Empty;
+
+        /// <summary>
+        /// QQ
+        /// </summary>
+        public string QQ { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 邮箱
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 微信号
+        /// </summary>
+        public string Wechat { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; } = string.Empty;
+
+    }
+}
---
+++ aspnet-core/src/CustomerRelationshipManagement.Application.Contracts/DTOS/CustomerProcessDtos/CustomerContacts/SearchCustomerContactDto.cs
@@ -0,0 +1,41 @@
+﻿using CustomerRelationshipManagement.Clues;
+using CustomerRelationshipManagement.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerRelationshipManagement.DTOS.CustomerProcessDtos.CustomerContacts
+{
+    public class SearchCustomerContactDto:PagingInfo
+    {
+        /// <summary>
+        /// 显示类型 0 显示所有 1 显示我负责的
+        /// </summary>
+        public int type { get; set; } = 0;
+
+        public Guid? AssignedTo { get; set; }        // 负责人
+
+        public DateTime? StartTime { get; set; }     // 时间范围起
+        public DateTime? EndTime { get; set; }       // 时间范围止
+
+        /// <summary>
+        /// 时间筛选类型：CreateTime | NextContact | LastFollow（创建、下次联系、最近跟进）
+        /// </summary>
+        public TimeField? TimeType { get; set; }
+
+        /// <summary>
+        /// 排序字段：CreateTime | NextContact | LastFollow（创建、下次联系、最近跟进）
+        /// </summary>
+        public TimeField? OrderBy { get; set; }
+
+        /// <summary>
+        /// 是否倒序
+        /// </summary>
+        public bool OrderDesc { get; set; } = true;
+
+        public string? Keyword { get; set; }
+
+    }
+}
